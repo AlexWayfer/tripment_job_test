949800a8ccd23165399c18535a3386870085a607
Alexander Popov <alex.wayfer@gmail.com>
2020-12-21 01:39:54 +0300

    Add specs for `Category` model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.create' do
+    subject do
+      described_class.create(name: 'Diagnostic')
+    end
+
+    it { is_expected.to be_valid }
+  end
 end
