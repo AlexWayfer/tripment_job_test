3419a7fa915fc3ac028d71bb495a0075fb8510eb
Alexander Popov <alex.wayfer@gmail.com>
2020-12-25 04:05:51 +0300

    Improve search request specs/docs a little

--- spec/requests/search_request_spec.rb
+++ spec/requests/search_request_spec.rb
@@ -12,22 +12,37 @@ RSpec.describe 'Searches', type: :request do
       get '/search', params: { query: query }
     end
 
-    ## cell, neuro
-    let(:query) { 'neuro' }
+    context 'with query string' do
+      ## I found suitable "cell", "neuro"
+      ## There are firstly mid-contains procedures in Wiki, and only after start-contains
+      let(:query) { 'neuro' }
 
-    it { is_expected.to have_http_status(:success) }
+      it { is_expected.to have_http_status(:success) }
 
-    describe 'body' do
-      subject { JSON.parse(super().body) }
+      describe 'body' do
+        subject { JSON.parse(super().body) }
 
-      let(:expected_procedures) do
-        [
-          include('name' => 'Neuroimaging'),
-          include('name' => 'Electroneuronography')
-        ]
+        let(:expected_procedures) do
+          [
+            include('name' => 'Neuroimaging'),
+            include('name' => 'Electroneuronography')
+          ]
+        end
+
+        it { is_expected.to match expected_procedures }
       end
-
-      it { is_expected.to match expected_procedures }
     end
+
+    ## I don't want to write specs for this case,
+    ## because there are different behavior for different environments
+    ## (correct 400 for production and exception for development and test)
+    ## and I don't want to stub different environments here neither write custom JSON errors
+    ## (I can, but don't see such point in task and 400 is OK when there is end-point documentation)
+
+    # context 'without query string' do
+    #   let(:query) { '' }
+    #
+    #   it { is_expected.to have_http_status(:bad_request) }
+    # end
   end
 end
