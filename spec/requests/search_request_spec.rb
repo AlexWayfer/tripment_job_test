9bc0396686162a46fe09e1a75e18f9f75ec2701a
Alexander Popov <alex.wayfer@gmail.com>
2020-12-25 03:55:18 +0300

    Implement search functionallity
    In 3 different ways: the first my own and two simpler and suggested by others.

--- spec/requests/search_request_spec.rb
+++ spec/requests/search_request_spec.rb
@@ -6,8 +6,28 @@ RSpec.describe 'Searches', type: :request do
   describe 'GET /' do
     subject { response }
 
-    before { get '/search' }
+    before do
+      system 'rake procedures:fetch'
+
+      get '/search', params: { query: query }
+    end
+
+    ## cell, neuro
+    let(:query) { 'neuro' }
 
     it { is_expected.to have_http_status(:success) }
+
+    describe 'body' do
+      subject { JSON.parse(super().body) }
+
+      let(:expected_procedures) do
+        [
+          include('name' => 'Neuroimaging'),
+          include('name' => 'Electroneuronography')
+        ]
+      end
+
+      it { is_expected.to match expected_procedures }
+    end
   end
 end
